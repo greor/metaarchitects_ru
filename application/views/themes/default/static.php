afd7f2f73e0f10ec6ac245ee8e141a5deb7a8d41
greor <gregor.zoonoid@gmail.com>
2016-05-04 21:16:27 +0300

    Яндекс карты
    Signed-off-by: greor <gregor.zoonoid@gmail.com>

--- application/views/themes/default/static.php
+++ application/views/themes/default/static.php
@@ -31,7 +31,36 @@
 				</article>
 			</div>
 		</div>
+<?php
+		if ($PAGE_ID == 1):
+?>		
+		<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+		<div class="container">
+			<div id="map" style="width: 100%; height: 500px"></div>
+			<script type="text/javascript">
+				ymaps.ready(init);
+				var myMap;
+				var myPlacemark;
+
+				function init(){     
+					myMap = new ymaps.Map("map", {
+						center: [59.9328, 30.3770],
+						zoom: 17,
+						controls: ["geolocationControl", "rulerControl", "zoomControl", "trafficControl"]
+					});
+
+					myPlacemark = new ymaps.Placemark([59.9328, 30.3770], { 
+			            hintContent: 'ООО "META"', 
+			            balloonContent: 'Архитектурная мастерская "META"' 
+			        });
+					myMap.geoObjects.add(myPlacemark);
+				}
+			</script>
+		</div>
+		<br>
+		<br>
 <?php 
+		endif;
 		echo View_Theme::factory('layout/share');
 ?>	
 	</section>
