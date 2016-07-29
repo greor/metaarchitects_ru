8779fbf70469d14c0f2d323cb52d070f162e10f1
Grisha Gudin <g.gudin@kubikrubik.ru>
2016-07-29 16:11:19 +0300

    Кликабельная ссылка телефона в футере
    Signed-off-by: Grisha Gudin <g.gudin@kubikrubik.ru>

--- application/views/themes/default/layout/footer/contacts.php
+++ application/views/themes/default/layout/footer/contacts.php
@@ -3,7 +3,7 @@
 	<div class="row">
 		<article class="col-md-12 text-center">
 			<p class="font4thin add-top-quarter" style="margin-top: 30px;">Санкт-Петербург, ул.Мытнинская, д.5/2<br>
-				<span class="hero-text font4thin black add-top-quarter">8-921-550-54-84</span>
+				<span class="hero-text font4thin black add-top-quarter"><a href="tel:+79215505484">8-921-550-54-84</a></span>
 			</p>
 			<h4 class="font1 dark color"><a href="mailto:info@ask-meta.ru">info@ask-meta.ru</a></h4>
 		</article>
